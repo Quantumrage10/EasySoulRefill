385db39b00e02659a4568e94c773c2cbb49edd7f
Acu1000 <140196544+Acu1000@users.noreply.github.com>
2023-07-23 02:07:07 +0200

    Update ModClass.cs

--- ModClass.cs
+++ ModClass.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using UObject = UnityEngine.Object;
 using Satchel;
 using System.Drawing.Printing;
+using IL;
+using System.Linq;
 
 namespace EasySoulRefill
 {
@@ -13,7 +15,18 @@ namespace EasySoulRefill
     {
         internal static EasySoulRefill Instance;
 
-        public override string GetVersion() => "v0.1.1.0";
+        public override string GetVersion() => "v0.1.1.3";
+
+        private string[] dream_boss_scene_names = {
+            "Dream_01_False_Knight",
+            "Dream_02_Mage_Lord",
+            "Dream_03_Infected_Knight",
+            "Dream_04_White_Defender",
+            "Dream_Mighty_Zote",
+            "dryya overworld",
+            "zemer overworld arena",
+            "hegemol overworld arena",
+            "isma overworld"};
 
         public EasySoulRefill() : base("FullSoulRespawn")
         {
@@ -23,21 +36,32 @@ namespace EasySoulRefill
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             ModHooks.SetPlayerBoolHook += OnSetBool;
+            On.GameManager.OnNextLevelReady += OnSceneLoad;
         }
 
         public bool OnSetBool(string name, bool orig)
         {
             if ((name == "atBench") && orig)
             {
-                Log("Benched");
                 RefillSoul();
             }
             return orig;
         }
 
+        
+        public void OnSceneLoad(On.GameManager.orig_OnNextLevelReady orig, global::GameManager self)
+        {
+            orig(self);
+            string scene_name = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+
+            if (dream_boss_scene_names.Contains(scene_name))
+            {
+                Satchel.CoroutineHelper.WaitForSecondsBeforeInvoke(0.5f, RefillSoul);
+            }
+        }
+
         public void RefillSoul()
         {
-            Log("Refilling");
             HeroController.instance.AddMPCharge(200);
         }
     }
